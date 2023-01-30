30c819ac7a72857dc9c3fe3fd518249430314c81
Stuart Clark <stuart.clark@spotlight.com>
2023-01-30 17:24:17 +0000

    Revert "Moved stuff about"
    This reverts commit fca14458c88606fe9c5fd0d70b096d9349817ac0.

--- utils/globalSetup.ts
+++ utils/globalSetup.ts
@@ -6,14 +6,12 @@ import * as dotenv from 'dotenv';
 export default async function globalSetup(config: FullConfig) {
 	console.log("Root Directory: " + config.rootDir);
 	if (process.env.test_env) {
-		let envConfigPath = `${config.rootDir}/env/.env.${process.env.test_env}`;
+		let envConfigPath = `${config.rootDir}/../env/.env.${process.env.test_env}`;
 		console.log("Using config path: " + envConfigPath);
 		dotenv.config({
 			path: envConfigPath,
 			override: true,
 		});
-	}else{
-		console.log("No test_env specified");		
 	}
 }
 
