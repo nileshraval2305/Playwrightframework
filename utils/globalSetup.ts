22aeeebca8c5c713ea901f8e136274aa52841445
Stuart Clark <stuart.clark@spotlight.com>
2023-01-30 16:32:37 +0000

    Set root path in globalSetup

--- utils/globalSetup.ts
+++ utils/globalSetup.ts
@@ -4,9 +4,10 @@ import type {FullConfig} from '@playwright/test';
 import * as dotenv from 'dotenv';
 
 export default async function globalSetup(config: FullConfig) {
+	console.log("Report path " + config.rootDir);
 	if (process.env.test_env) {
 		dotenv.config({
-			path: `env/.env.${process.env.test_env}`,
+			path: `${config.rootDir}/env/.env.${process.env.test_env}`,
 			override: true,
 		});
 	}
