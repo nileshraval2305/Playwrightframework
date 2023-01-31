8964397a0ab074da0ed22b7992b19a2f3e9eaac9
nilesh-raval <nilesh.raval@castingnetworks.com>
2023-01-31 15:33:23 +0530

    yml file is updated

--- utils/globalSetup.ts
+++ utils/globalSetup.ts
@@ -4,10 +4,10 @@ import * as dotenv from 'dotenv';
 import * as path from 'path';
 
 export default async function globalSetup(config: FullConfig) {
-	if (process.env.test_env) {		
+	if (process.env.test_env) {
 		const envConfigPath = path.join(process.cwd(), `env/.env.${process.env.test_env}`);
 
-		console.log("Using config path: " + envConfigPath);
+		console.log('Using config path: ' + envConfigPath);
 		dotenv.config({
 			path: envConfigPath,
 			override: true,
